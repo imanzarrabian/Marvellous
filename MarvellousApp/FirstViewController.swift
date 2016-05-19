24a5daa966ecf711da27d8ea387539bba69579b9
Iman Zarrabian <iman@omts.fr>
2016-05-19 16:47:13 +0200

    Starter project

---
+++ MarvellousApp/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  MarvellousApp
+//
+//  Created by Iman Zarrabian on 19/05/16.
+//  Copyright © 2016 Web School Factory. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ MarvellousApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MarvellousApp
+//
+//  Created by Iman Zarrabian on 19/05/16.
+//  Copyright © 2016 Web School Factory. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
